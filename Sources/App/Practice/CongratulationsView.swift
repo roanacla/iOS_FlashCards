6c8708367a611c66996a40566896dd39b2c5ab59
roanacla <roanacla@me.com>
2020-06-03 12:44:51 -0700

    Play with V-Stacks, H-Stacks and Z-Stacks

--- Sources/App/Practice/CongratulationsView.swift
+++ Sources/App/Practice/CongratulationsView.swift
@@ -31,13 +31,58 @@ import SwiftUI
 struct CongratulationsView: View {
   let avatarSize: CGFloat = 120
   let userName: String
+    @EnvironmentObject var challengesViewModel: ChallengesViewModel
   
   init(userName: String) {
     self.userName = userName
   }
+    
+    func printSomething() {
+        print("Hello")
+    }
   
   var body: some View {
-    EmptyView()
+    VStack {
+        Spacer()
+        Text("Congratulations")
+            .font(.title)
+            .foregroundColor(.gray)
+        ZStack {
+            VStack(spacing: 0) {
+                Rectangle()
+                    .frame(height: 90)
+                    .foregroundColor(Color(red: 0.5, green: 0, blue: 0, opacity: 0.2))
+                Rectangle()
+                    .frame(height: 90)
+                    .foregroundColor(Color(red: 0.6, green: 0.1, blue: 0.1, opacity: 0.4))
+            }
+            Image(systemName: "person.fill")
+                .resizable()
+                .padding()
+                .frame(width: avatarSize, height: avatarSize)
+                .background(Color.white.opacity(0.5))
+                .cornerRadius(avatarSize/2, antialiased: true)
+                .shadow(radius: 4)
+            VStack () {
+                Spacer()
+                Text(self.userName)
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .shadow(radius: 7)
+            }.padding()
+        }.frame(height: 180)
+        Text("You're awesome!")
+            .fontWeight(.bold)
+            .foregroundColor(.gray)
+        Spacer()
+        Button(action: {
+            self.challengesViewModel.restart()
+        }, label: {
+            Text("Play Again")
+        })
+        .padding()
+    }
   }
 }
 
